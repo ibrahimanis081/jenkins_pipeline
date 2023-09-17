320fedd723847829e17f8b8157e8e4c398824598
Anees <ibrahimanis081@gmail.com>
2023-09-17 17:54:25 +0100

    Fixed failing test

--- app/test_app.py
+++ app/test_app.py
@@ -11,7 +11,7 @@ class TestApp(unittest.TestCase):
     def test_home_page(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), "Hello, orld!")
+        self.assertEqual(response.data.decode('utf-8'), "Hello, World!")
 
 if __name__ == '__main__':
     unittest.main()
